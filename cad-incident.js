accd6921b1b3451d746fd96fcc90b7f088fcceb2
Marius Ursache <marius@marius.me.uk>
2019-02-18 16:20:12 +1100

    Added CAD incident schema

---
+++ cad-incident.js
@@ -0,0 +1,412 @@
+{
+  "title": "Incident",
+  "description": "CAD Incident",
+  "type": "object",
+  "properties": {
+
+    // Heartbeat
+    "Time": {
+      "type": ["string", "number"]
+    },
+    "Status": {
+      "type": "string"
+    },
+    "Info": {
+      "type": "string"
+    },
+    "Message": {
+      "type": "string"
+    },
+
+    // Incident Specific Info
+    "AgencyID": {
+      "type": "string"
+    },
+    "TransactionID": {
+      "type": ["string", "number"]
+    },
+    "IncidentNumber": {
+      "type": "string"
+    },
+    "AgencyIncidentCallTypeDescription": {
+      "type": "string"
+    },
+    "AgencyIncidentCallType": {
+      "type": "string"
+    },
+    "AgencyIncidentCallSubTypeDescription": {
+      "type": "string"
+    },
+    "AgencyIncidentCallSubType": {
+      "type": "string"
+    },
+    "PulsePointIncidentCallType": {
+      "type": "string"
+    },
+    "AgencyIncidentPriorityDescription": {
+      "type": "string"
+    },
+    "AlarmLevel": {
+      "type": "string"
+    },
+    "CommandName": {
+      "type": "string"
+    },
+    "FireMap": {
+      "type": "string"
+    },
+    "TBMap": {
+      "type": "string"
+    },
+    "MapPages": {
+      "type": "string"
+    },
+
+    // Debugging
+    "Interface": {
+      "type": "string"
+    },
+    "InterfaceVersion": {
+      "type": "string"
+    },
+
+    // Dates
+    "EntryDateTime": {
+      "type": "string"
+    },
+    "ClosedDateTime": {
+      "type": ["string", "null"]
+    },
+    "CallReceivedDateTime": {
+      "type": "string"
+    },
+    "DispatchDateTime": {
+      "type": "string"
+    },
+    "IncidentLastUpdate": {
+      "type": "string"
+    },
+    "EnrouteDateTime": {
+      "type": "string"
+    },
+    "OnSceneDateTime": {
+      "type": "string"
+    },
+
+    // Communication Channels
+    "TacticalChannel": {
+      "type": "string"
+    },
+    "TacticalAltChannel": {
+      "type": "string"
+    },
+    "CommandChannel": {
+      "type": "string"
+    },
+
+    // Address Related
+    "StreetName": {
+      "type": "string"
+    },
+    "StreetNumber": {
+      "type": "string"
+    },
+    "StreetSuffix": {
+      "type": "string"
+    },
+    "Predirectional": {
+      "type": "string"
+    },
+    "Postdirectional": {
+      "type": "string"
+    },
+    "Building": {
+      "type": "string"
+    },
+    "Suite": {
+      "type": "string"
+    },
+    "Apartment": {
+      "type": "string"
+    },
+    "CityOrLocality": {
+      "type": "string"
+    },
+    // Remap to CityOrLocality
+    "City": {
+      "type": "string"
+    },
+    "County": {
+      "type": "string"
+    },
+    "StateOrProvince": {
+      "type": "string"
+    },
+    // Remap to StateOrProvince
+    "StateOfProvince": {
+      "type": "string"
+    },
+    "PostalCode": {
+      "type": "string"
+    },
+    "CommonPlaceName": {
+      "type": "string"
+    },
+    "LocationComment": {
+      "type": "string"
+    },
+    "CrossStreet1": {
+      "type": "string"
+    },
+    "CrossStreet2": {
+      "type": "string"
+    },
+    "CrossStreetName": {
+      "type": "string"
+    },
+
+    // Lat/Long
+    "Latitude": {
+      "type": ["string", "number"]
+    },
+    "Longitude": {
+      "type": ["string", "number"]
+    },
+
+    // Unit
+    "Unit": {
+      "type": "array",
+      "maxItems": 10000,
+      "items": {
+        "title": "Unit",
+        "description": "Unit Assigned to the incident",
+        "type": "object",
+        "properties": {
+          "UnitDispatchNumber": {
+            "type": ["string", "number"],
+            "maxLength": 128
+          },
+          "UnitID": {
+            "type": "string",
+            "maxLength": 128
+          },
+          "TimeDispatched": {
+            "type": "string"
+          },
+          "TimeEnroute": {
+            "type": "string"
+          },
+          "TimeStaged": {
+            "type": "string"
+          },
+          "TimeArrived": {
+            "type": "string"
+          },
+          "TimeCleared": {
+            "type": "string"
+          },
+          "TimeAtHospital": {
+            "type": "string"
+          },
+          "TimePatient": {
+            "type": "string"
+          },
+          "TimeTransport": {
+            "type": "string"
+          },
+          "TimeTransporting": {
+            "type": "string"
+          }
+        }
+      }
+    },
+
+    // Comment
+    "Comment": {
+      "type": "array",
+      "maxItems": 100000,
+      "items": {
+        "title": "Comment",
+        "description": "Comment associated with the incident",
+        "type": "object",
+        "properties": {
+          "Comment": {
+            "type": "string",
+            "maxLength": 10000
+          },
+          "CommentSource": {
+            "type": "string",
+            "maxLength": 256
+          },
+          "CommentDateTime": {
+            "type": "string"
+          }
+        }
+      }
+    },
+
+    // Anaheim specific
+    "ReportNumber": {
+      "type": ["string", "array"],
+      "maxItems": 150,
+      "items": {
+        "title": "Report Number",
+        "description": "Associated Report Number",
+        "type": "string"
+      }
+    },
+
+    // Santa Clara
+    "AgencyDeterminantCode": {
+      "type": "string"
+    },
+    "AgencyIncidentCategory": {
+      "type": "string"
+    },
+    "CFMap": {
+      "type": "string"
+    },
+    "CallSource": {
+      "type": "string"
+    },
+    "CaseNumber": {
+      "type": "string"
+    },
+    "CrossRefDataOwnerAgyID1": {
+      "type": "string"
+    },
+    "CrossRefDataOwnerAgyID2": {
+      "type": "string"
+    },
+    "CrossRefDataSubmitterAgyID1": {
+      "type": "string"
+    },
+    "CrossRefDataSubmitterAgyID2": {
+      "type": "string"
+    },
+    "CrossRefEventCategory1": {
+      "type": "string"
+    },
+    "CrossRefEventCategory2": {
+      "type": "string"
+    },
+    "CrossRefEventNum1": {
+      "type": "string"
+    },
+    "CrossRefEventNum2": {
+      "type": "string"
+    },
+    "EventLevel": {
+      "type": "string"
+    },
+    "EventNumber": {
+      "type": "string"
+    },
+    "FireManagementZone": {
+      "type": "string"
+    },
+    "FirstDueStation": {
+      "type": "string"
+    },
+    "MedicalAgencyIncidentType": {
+      "type": "string"
+    },
+    "MedicalEventCode": {
+      "type": "string"
+    },
+    "PopulationDensity": {
+      "type": "string"
+    },
+    "Priority": {
+      "type": "string"
+    },
+    "WorkstationID": {
+      "type": "string"
+    },
+    "LatitudeDegree": {
+      "type": ["string", "number"]
+    },
+    "LatitudeMinute": {
+      "type": ["string", "number"]
+    },
+    "LatitudeSecond": {
+      "type": ["string", "number"]
+    },
+    "LongitudeDegree": {
+      "type": ["string", "number"]
+    },
+    "LongitudeMinute": {
+      "type": ["string", "number"]
+    },
+    "LongitudeSecond": {
+      "type": ["string", "number"]
+    },
+    "Location": {
+      "type": "string"
+    },
+    "LocationType": {
+      "type": "string"
+    },
+    "OrigLocation": {
+      "type": "string"
+    },
+    "Jurisdiction": {
+      "type": "string"
+    },
+    "EMDDeterminantCompleteDateTime": {
+      "type": "string"
+    },
+
+    // Tablet Command
+    // Station?
+    "station": {
+      "type": "array",
+      "properties": {
+        "code": {
+          "type": "string"
+        },
+        "name": {
+          "type": "string"
+        }
+      }
+    },
+
+    // Leaked
+    "entity$": {
+      "type": "string"
+    },
+    "queued_at": {
+      "type": "number"
+    },
+    "scheduled_at": {
+      "type": "number"
+    },
+    "admin_note": {
+      "type": "string"
+    },
+    "modified_date": {
+      "type": "string"
+    },
+    "modified_unix_date": {
+      "type": "number"
+    },
+    "preference_location": {
+      "type": "string"
+    },
+    "cross_streets": {
+      "type": "string"
+    },
+    "CADSimulator": {
+      "type": "string"
+    },
+    "full_address": {
+      "type": "string"
+    }
+
+  },
+  "required": [
+    "AgencyID",
+    "TransactionID",
+    "IncidentNumber"
+  ]
+};

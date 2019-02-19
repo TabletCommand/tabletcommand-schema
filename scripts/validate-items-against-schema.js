6416fe3cf8bd7d6fa211e08f05f7023f9e3b5e32
Marius Ursache <marius@marius.me.uk>
2019-02-19 12:52:37 +1100

    Added reportAdditionalProperties

--- scripts/validate-items-against-schema.js
+++ scripts/validate-items-against-schema.js
@@ -3,16 +3,21 @@
 const fs = require("fs");
 const schema = require("../cad-incident.js");
 
+const _ = require("lodash");
 const Ajv = require("ajv");
+const hdiff = require("hdiff");
 
 const input = "./sample.json";
 
-let errors = [];
-
-const ajv = new Ajv({
+const validator = new Ajv({
   allErrors: true,
   jsonPointers: true,
   verbose: true
+});
+
+const reporter = new Ajv({
+  removeAdditional: true,
+  allErrors: true
 });
 
 const lineReader = require("readline").createInterface({
@@ -21,19 +26,57 @@ const lineReader = require("readline").createInterface({
 
 lineReader.on("line", function(line) {
   const item = JSON.parse(line);
-  validate(schema, item.postBody);
+  const incident = item.postBody;
+  return validate(schema, incident, (isValid) => {
+    if (!isValid) {
+      process.exit(1);
+    }
+    return reportAdditionalProperties(schema, incident, (hasChanges) => {
+      if (hasChanges) {
+        process.exit(2);
+      }
+    });
+  });
 });
 
 lineReader.on("close", function() {
   console.log("Validation completed!");
 });
 
-function validate(schema, item) {
-  let errors = [];
-  const valid = ajv.validate(schema, incident);
+function validate(schema, item, callback) {
+  const valid = validator.validate(schema, item);
   if (!valid) {
-    errors = _.clone(ajv.errors);
+    validator.errors.forEach((e) => {
+      console.log(e.dataPath, e.message, "got:", e.data);
+    });
+    return callback(valid);
   }
 
-  console.log(valid, item);
+  return callback(valid);
+}
+
+function reportAdditionalProperties(schema, item, callback) {
+  var delta = {};
+  var hasChanges = false;
+
+  let itemSchema = _.clone(schema);
+  itemSchema.additionalProperties = false;
+
+  const original = _.clone(item);
+  reporter.validate(itemSchema, item);
+  const cleanItem = _.clone(item);
+
+  const diff = hdiff(original, cleanItem, {
+    unchanged: false
+  });
+
+  if (_.isObject(diff) && _.size(diff) > 0) {
+    delta = _.clone(diff);
+    hasChanges = true;
+    console.log("Item:\n", JSON.stringify(original));
+    console.log("Clean Item:\n", JSON.stringify(cleanItem));
+    console.log("Delta:\n", JSON.stringify(delta));
+  }
+
+  return callback(hasChanges, delta);
 }
